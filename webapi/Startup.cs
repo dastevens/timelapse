bb84d4140f24307372805dce37222a52690a0b2c
dstevens <david.stevens@res-group.com>
2019-08-29 22:26:38 +0200

    Add CORS for dev

--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -27,6 +27,13 @@ namespace webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             var fileSystem = new FileSystem();
@@ -40,21 +47,20 @@ namespace webapi
             return Configuration.GetValue<string>($"AppSettings:{key}");
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors("AllowAll");
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseDefaultFiles();
+                app.UseStaticFiles();
             }
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
             app.UseMvc();
         }
     }
