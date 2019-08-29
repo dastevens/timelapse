5abfed0e64573ff1eee8ab2b3c01faab72b4a81d
dstevens <david.stevens@res-group.com>
2019-08-29 16:20:45 +0200

    Client project list loads, preview works 2

--- client/src/redux/ControlProjectAction.ts
+++ client/src/redux/ControlProjectAction.ts
@@ -4,8 +4,6 @@ import wrapAsyncWorker from './wrapAsyncWorker';
 import { ProjectID } from '../model/Project';
 import {
     previewProject as apiPreviewProject,
-    startProject as apiStartProject,
-    stopProject as apiStopProject,
 } from '../api/Projects';
 
 const actionCreator = actionCreatorFactory();
@@ -14,17 +12,7 @@ export const PreviewAction = actionCreator.async<ProjectID, string>('Preview');
 const previewWorker = wrapAsyncWorker<ProjectID, string, void>(
     PreviewAction, (params: ProjectID): Promise<string> => apiPreviewProject(params.name)
 );
-export const StartAction = actionCreator.async<ProjectID, void>('Start');
-const startWorker = wrapAsyncWorker<ProjectID, void, void>(
-    StartAction, (params: ProjectID): Promise<void> => apiStartProject(params.name)
-);
-export const StopAction = actionCreator.async<ProjectID, void>('Stop');
-const stopWorker = wrapAsyncWorker<ProjectID, void, void>(
-    StopAction, (params: ProjectID): Promise<void> => apiStopProject(params.name)
-);
 
 export const controlProject = {
     preview: (name: string) => (dispatch: Dispatch<AnyAction>) => previewWorker(dispatch, { name }),
-    start: (name: string) => (dispatch: Dispatch<AnyAction>) => startWorker(dispatch, { name }),
-    stop: (name: string) => (dispatch: Dispatch<AnyAction>) => stopWorker(dispatch, { name }),
 };
